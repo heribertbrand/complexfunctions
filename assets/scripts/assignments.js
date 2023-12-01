33c00988a9163865b74fc04a198cbd06f265229f
Heribert <crossbrand96@gmail.com>
2023-12-01 07:10:41 +0100

    modify linters

--- assets/scripts/assignments.js
+++ assets/scripts/assignments.js
@@ -2,7 +2,7 @@
 // Q 1: CHECK FOR 'EVEN-NESS' OR 'ODD-NESS'
 
 const isEven = (m) => {
-  if (m % 2 == 0) {
+  if (m % 2 === 0) {
     return `${m} is an even number`
   }
   return `${m} is an odd number`
@@ -10,12 +10,11 @@ const isEven = (m) => {
 
 console.log(isEven(55))
 
-//Q 2: COUNT UPPERCASES
+// Q 2: COUNT UPPERCASES
 
 const countChar = (str) => {
   let count = 0
   for (let i = 0; i <= str.length; i++) {
-
     if (str[i] >= 'A' && str[i] <= 'Z') {
       count++
     }
@@ -28,7 +27,7 @@ console.log(countChar('JavaScripT'))
 // Q 3: COUNT SPECIFIC UPPERCASE
 
 const countBs = (str, char) => {
-  let count = 0
+  const count = 0
   for (let i = 0; i <= str.length - 1; i++) {
     if (str[i] === char) {
       count++
@@ -41,12 +40,10 @@ console.log(countBs('truthy AND falsy', 'A'))
 
 // Q 4: FIBONACCI
 
-
-
 // Q 5: RANGE
 
 const range = (start, end) => {
-  let count = []
+  const count = []
   // end = count[count.length - 1]
   for (let i = start; i <= end; i++ ) {
     // count++
@@ -61,7 +58,7 @@ console.log(range(2, 4))
 // Q 6: STEP-RANGE (returns fatal error...when tested for a decreament)
 
 const stepRange = (start, end, incri) => {
-  let count = []
+  const count = []
   // end = count[count.length - 1]
   for (let i = start; i <= end; i += incri) {
     // if (start++ || end--) {... 
@@ -74,20 +71,15 @@ const stepRange = (start, end, incri) => {
 
 console.log(stepRange(1, 15, 4)) // =>
 
-
 // Q 7: TRUE-RANGE
-
-
 
 // Q 8: RETURN THE SUM OF THE ARRAY NUMBERS
 
 const arraySum = (arr) => {
-
   // count from zero not an empty array...
-  let accumulator = 0
+  const accumulator = 0
   let looped = arr.length - 1
   for (let i = 0; i <= looped; i++) {
-
     // accum.push(i) xxx
     accumulator += arr[i]
   }
@@ -97,11 +89,10 @@ const arraySum = (arr) => {
 let theArr = [1, 2, 3]
 console.log(arraySum(theArr)) // => 6
 
-
 // Q 9: RETURN THE AVERAGE OF THE ARRAY NUMBERS
 
 const arrAvr = (list) => {
-  let count = 0
+  const count = 0
   let stretch = list.length - 1
   for (let i = 0; i <= stretch; i++) {
     count += list[i]
@@ -109,10 +100,8 @@ const arrAvr = (list) => {
   }
   return count / (list.length)
 }
-
 let arre = [20, 30, 40, 50]
 console.log(arrAvr(arre)) // => 35
-
 
 // Q 10: 
 
@@ -123,15 +112,12 @@ const deepEqual = (a, b) => {
   } else {
     return false
   }
-  
 }
-
 console.log(deepEqual('3', 3)) // => true
-
 // 
 
-const obj1 = new Object()
-const obj2 = new Object()
+const obj1 = Object{}
+const obj2 = Object{}
 obj1.name = 'terry'
 obj1.city = 'bertoua'
 obj2.name = 'terry'
